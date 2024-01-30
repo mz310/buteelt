00b9fd6972d0e1bd89cf6dd179c142a7239c9ac4
Puujee_Uni <Puujee_Uni@172.16.152.46>
2024-01-30 14:28:16 +0800

    hha

--- Circle/src/labs/circle.java
+++ Circle/src/labs/circle.java
@@ -1,21 +1,13 @@
 package labs;
-import java.util.Scanner;
 public class circle {
- private static final double PI = 3.14;
- private final double radius;
- public circle(double radius) {
- this.radius = radius;
- }
- public static void main(String[] args) {
- System.out.println("------------------------------------------------------------");
- System.out.println("CircleCalc v1.0");
- System.out.println();
- System.out.println("Calculates and prints information for a user-supplied radius");
- System.out.println("------------------------------------------------------------");
- System.out.print("Enter the circle's radius: ");
- Scanner in = new Scanner(System.in);
- circle c = new circle(in.nextFloat());
- // TODO: compute and print the circumference and area of the circle
- }
-}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+		System.out.println("------------------------------------------------------------");
+		 System.out.println("CircleCalc v1.0");
+		 System.out.println();
+		 System.out.println("Calculates and prints information for a user-supplied radius");
+		 System.out.println("------------------------------------------------------------");
+	}
 
+}
