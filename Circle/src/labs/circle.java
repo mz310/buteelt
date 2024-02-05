eaf7af6f362956d827e0c4d65b508630efd71980
Puujee_Uni <Puujee_Uni@192.168.1.165>
2024-02-05 20:51:37 +0800

    changed a lot

--- Circle/src/labs/circle.java
+++ Circle/src/labs/circle.java
@@ -1,11 +1,19 @@
 package labs;
+import java.util.Queue;
+import java.util.LinkedList;
 public class circle {
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
-		System.out.println("Hello111");
-		System.out.println("2024-05");
+		Queue<String> queue = new LinkedList<String>();
+		queue.offer("KAren");
+		queue.offer("Chad");
+		queue.offer("Steve");
+		queue.offer("Harold");
+		System.out.println(queue);
+		
 	}
 
 }
+
 //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
